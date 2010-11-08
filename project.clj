73eb436b5846d99135a789e8b65ee7abb7c9f55c
Kyle Burton <kburton@relaynetwork.com>
2010-11-08 18:15:24 -0500

    bumped clojure/contrib to 1.2.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Bloom Filter implementation in Clojure, see also: http://github.com/kyleburton/clj-bloom"
   :warn-on-reflection true
   :dependencies
-  [[org.clojure/clojure "1.1.0"]
-   [org.clojure/clojure-contrib "1.1.0"]]
+  [[org.clojure/clojure "1.2.0"]
+   [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies
   [[swank-clojure "1.2.1"]])
