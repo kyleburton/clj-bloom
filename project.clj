c6ee74422458c240c5f25e639196da30dce8ac9d
Kyle Burton <kyle.burton@gmail.com>
2011-11-15 21:54:08 -0500

    remove autodoc dep; up to clojure 1.2.1; local classpath; simplify the run.sh

--- project.clj
+++ project.clj
@@ -1,15 +1,9 @@
 (defproject com.github.kyleburton/clj-bloom "1.0.2"
   :description "Bloom Filter implementation in Clojure, see also: http://github.com/kyleburton/clj-bloom"
   :warn-on-reflection true
+  :local-repo-classpath true
   :dependencies
-  [[org.clojure/clojure "1.2.0"]
+  [[org.clojure/clojure "1.2.1"]
    [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies
-  [[swank-clojure "1.2.1"]
-  [autodoc "0.7.1"]]
-  :autodoc {
-    :name "clj-bloom"
-    :page-title "clj-bloom: API Documentation"
-    :description "Bloom Filter"
-    :web-home "http://kyleburton.github.com/projects/clj-bloom/"
-  })
+  [[swank-clojure/swank-clojure "1.4.0-SNAPSHOT"]])
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 set -e
 set -x
 lein deps
-test -f clj-bloom.jar            || lein jar      && lein test
-test -f clj-bloom-standalone.jar || lein uberjar
 
-java -cp clj-bloom-standalone.jar clojure.main examples/words.clj
+lein classpath > .classpath
+
+java -cp $(cat .classpath) clojure.main examples/words.clj
