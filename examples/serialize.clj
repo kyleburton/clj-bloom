40d96d09407074bba3cd308e70dbc6af2943f204
Kyle Burton <kyle.burton@gmail.com>
2014-08-16 11:45:47 -0400

    updated serialization example

--- examples/serialize.clj
+++ examples/serialize.clj
@@ -3,15 +3,20 @@
    [clojure.java.io                 :as io]
    [com.github.kyleburton.clj-bloom :as bloom]])
 
+(defonce num-hashes 7)
+(defonce num-expected-entries 2875518)
 
 ;; the 7 is the number of hashes to use, these settings should result
 ;; in about a 1% error rate
+(defn make-hash-fn []
+  (bloom/make-permuted-hash-fn
+   bloom/make-hash-fn-hash-code
+   (map str (range 0 num-hashes))))
+
 (defn make-filter []
   (bloom/bloom-filter
-   2875518 ;; estimated number of of entries
-   (bloom/make-permuted-hash-fn
-    bloom/make-hash-fn-hash-code
-    (map str (range 0 7)))))
+   num-expected-entries
+   (make-hash-fn)))
 
 (defn populate-with-words [flt]
   (with-open [rdr (io/reader "/usr/share/dict/words")]
@@ -19,30 +24,55 @@
      (doseq [word (line-seq rdr)]
        (bloom/add! flt word)))))
 
-(defn bitset->file [bitset fname]
+(defn filter->object-output-stream [^java.util.Map flt ^java.io.ObjectOutputStream oos]
+  (.writeObject oos (:num-bits flt))
+  (.writeObject oos @(:insertions flt))
+  (.writeObject oos (:bitarray flt)))
+
+(defn object-input-stream->filter [^java.io.ObjectInputStream ois hash-fn]
+  (let [[num-bits insertions bitarray] [(.readObject ois) (.readObject ois) (.readObject ois)]]
+    {:bitarray   bitarray
+     :hash-fn    hash-fn
+     :num-bits   num-bits
+     :insertions (atom insertions)}))
+
+;; An issue with serializing filters is the :hash-fn, it would need to
+;; be re-constructed on de-serialization, passing the configuration
+;; that created the hash functions would be a good way of doing that
+;; but is not a core concern of the clj-bloom library and probably
+;; shouldn't be(?).  A reasonable approach is to not serialize the
+;; function, but allow it to be re-provided on deserialization.
+(defn filter->file [flt ^String fname]
   (with-open [oos (java.io.ObjectOutputStream. (java.io.FileOutputStream. fname))]
-    (.writeObject oos bitset)))
+    (filter->object-output-stream flt oos)))
 
-(defn file->bitset [fname]
+;; NB: you must provide an equivalent hash-fn on deserialization
+(defn file->filter [^String fname hash-fn]
   (with-open [ois (java.io.ObjectInputStream. (java.io.FileInputStream. fname))]
-    (.readObject ois)))
+    (object-input-stream->filter ois hash-fn)))
 
-(defn main []
-  (let [filter1 (make-filter)]
-    (time
-     (populate-with-words filter1))
-    (bitset->file (:bitarray filter1) "bitset.dat")
-    (let [filter2 (assoc (make-filter)
-                    :bitarray (file->bitset "bitset.dat"))]
-      (println (format "is 'orange' in filter1? %s" (bloom/include? filter1 "orange")))
-      (println (format "is 'orange' in filter2? %s" (bloom/include? filter2 "orange"))))))
+(defn test-full-filter-serialization []
+  (let [fname "f1.dat"
+        f1    (doto
+                  (make-filter)
+                populate-with-words)]
+    (filter->file f1 fname)
+    (let [f2 (file->filter fname (make-hash-fn))]
+      (printf "f1.cardinality=%d\n" (-> f1 :bitarray .cardinality))
+      (printf "f2.cardinality=%d\n" (-> f1 :bitarray .cardinality))
+      (printf "f1 contains 'orange'? %s\n" (bloom/include? f1 "orange"))
+      (printf "f2 contains 'orange'? %s\n" (bloom/include? f2 "orange")))))
+
+(defn -main [& args]
+  (test-full-filter-serialization))
 
 (comment
-  (main)
-  ;; "Elapsed time: 1489.591 msecs"
-  ;; is 'orange' in filter1? true
-  ;; is 'orange' in filter2? true
-
-
+  (time
+   (test-full-filter-serialization))
+  ;; f1.cardinality=616322
+  ;; f2.cardinality=616322
+  ;; f1 contains 'orange'? true
+  ;; f2 contains 'orange'? true
+  ;; "Elapsed time: 791.19503 msecs"
 
   )
--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :repositories         {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :local-repo-classpath true
   :profiles             {:dev {:dependencies [[swank-clojure "1.4.3"]
-                                              [org.clojure/clojure "1.5.1"]
+                                              [org.clojure/clojure "1.6.0"]
                                               [org.clojure/math.combinatorics "0.0.4"]]
                                :resource-paths ["examples"]}
                          ;; NB: the use of ex-info prevents 1.3 from being supported
@@ -21,7 +21,7 @@
                                               ]}
                          :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]
                                               ]}
-                         :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]
+                         :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]
                                               ]}}
   :aliases              {"all" ["with-profile" "dev,1.2:dev,1.3:dev,1.4:dev,1.5:dev,1.6"]}
   :global-vars          {*warn-on-reflection* true}
